0507e9e33d6029f0af5ad81d739a1e7fca33e4ef
vivekreddy <vivekvardhan.s@gmail.com>
2023-05-18 12:44:41 +0530

    first commit

---
+++ quiz/quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizApp.swift
+//  quiz
+//
+//  Created by Vivek on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
